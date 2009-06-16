c20437094f1ad8d4ac240a91facf26b804d2af14
Paolo Lucente <pl+github@pmacct.net>
2009-06-16 19:42:19 +0000

    * Commit 20090616-2 (forgotten bgp.h)

--- src/bgp/bgp.h
+++ src/bgp/bgp.h
@@ -146,7 +146,7 @@ EXT int bgp_marker_check(struct bgp_header *, int);
 EXT int bgp_keepalive_msg(char *);
 EXT int bgp_open_msg(char *, char *, int, struct bgp_peer *);
 EXT int bgp_update_msg(struct bgp_peer *, char *);
-EXT int bgp_attr_parse(struct bgp_peer *, void *, char *, int, struct bgp_nlri *, struct bgp_nlri *);
+EXT int bgp_attr_parse(struct bgp_peer *, struct bgp_attr *, char *, int, struct bgp_nlri *, struct bgp_nlri *);
 EXT int bgp_attr_parse_community(struct bgp_peer *, u_int16_t, struct bgp_attr *, char *, u_int8_t);
 EXT int bgp_attr_parse_ecommunity(struct bgp_peer *, u_int16_t, struct bgp_attr *, char *, u_int8_t);
 EXT int bgp_attr_parse_aspath(struct bgp_peer *, u_int16_t, struct bgp_attr *, char *, u_int8_t);
