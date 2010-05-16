708e2b7b622f75ba472a458b3db4c0e2876d1505
Paolo Lucente <pl+github@pmacct.net>
2010-05-16 10:24:39 +0000

    * fix, sql_common.c: maximum SQL writers warning message was never reached   unless a recovery method is specifited. Thanks to Sergio Charpinel Jr for   reporting the issue. * Documentation

--- src/sql_common.c
+++ src/sql_common.c
@@ -351,7 +351,8 @@ int sql_cache_flush(struct db_cache *queue[], int index, struct insert_data *ida
     /* If we are very near to our maximum writers threshold, let's resort to any configured
        recovery mechanism - SQL_CACHE_COMMITTED => SQL_CACHE_ERROR; otherwise, will proceed
        as usual */
-    if (sql_writers.active == config.sql_max_writers-1) {
+    if ((sql_writers.active == config.sql_max_writers-1) &&
+	(config.sql_backup_host || config.sql_recovery_logfile)) {
       for (j = 0; j < index; j++) {
 	if (queue[j]->valid == SQL_CACHE_COMMITTED) queue[j]->valid = SQL_CACHE_ERROR;
       }
