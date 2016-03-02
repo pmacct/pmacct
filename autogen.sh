8cb154303af92974156631551dd13e793413a797
Vincent Bernat <vincent@bernat.im>
2016-03-02 13:27:36 +0100

    * build: use pkg-config to find most dependencies
    Using PKG_CHECK_MODULE macro greatly reduces the amount of code needed
    to check a library. Moreover, it is usually more reliable. For example,
    the PostgreSQL library headers are found without requiring anything
    more. Dependencies with other libraries are also automatically
    found. Appropriate CFLAGS are used (for example, a library whose ABI
    depends on CFLAGS, like -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64).
    
    This also removes options to override any path. A user can still
    easily (but this may need to be documented) override what pkg-config
    finds. For example, if one wanted to use a statically compiled jansson
    library, it's possible to use:
    
       ./configure --enable-jansson \
           pkg_cv_JANSSON_LIBS='/some/location/libjansso.a' \
           pkg_cv_JANSSON_CFLAGS='-I/some/location/include'
    
    MySQL doesn't have a pkg-config configuration file. Instead, rely on
    mysql_config program.
    
    MongoDB support seems currently broken. mongoc library doesn't come with
    a mongo.h header file. Maybe it requires an old version.

--- autogen.sh
+++ autogen.sh
@@ -30,12 +30,38 @@ check_command "$AUTOCONF"
 check_command "$AUTOHEADER"
 check_command "$AUTOMAKE"
 
+# Absence of pkg-config or misconfiguration can make some odd error
+# messages, we check if it is installed correctly. See:
+#  https://blogs.oracle.com/mandy/entry/autoconf_weirdness
+#
+# We cannot just check for pkg-config command, we need to check for
+# PKG_* macros. The pkg-config command can be defined in ./configure,
+# we cannot tell anything when not present.
+check_pkg_config() {
+    grep -q '^AC_DEFUN.*PKG_CHECK_MODULES' aclocal.m4 || {
+        cat <<EOF >&2
+autogen.sh: could not find PKG_CHECK_MODULES macro.
+
+  Either pkg-config is not installed on your system or
+  \`pkg.m4' is missing or not found by aclocal.
+
+  If \`pkg.m4' is installed at an unusual location, re-run
+  \`autogen.sh' by setting \`ACLOCAL_FLAGS':
+
+    ACLOCAL_FLAGS="-I <prefix>/share/aclocal" ./autogen.sh
+
+EOF
+        exit 1
+    }
+}
+
 echo "autogen.sh: reconfigure with autoreconf"
 ${AUTORECONF} -vif -I m4 || {
     echo "autogen.sh: autoreconf has failed ($?), let's do it manually"
     [ -f ./configure.ac ] || [ -f ./configure.in ] || continue
     echo "autogen.sh: configure `basename $PWD`"
     ${ACLOCAL} -I m4 ${ACLOCAL_FLAGS}
+    check_pkg_config
     ${LIBTOOLIZE} --automake --copy --force
     ${ACLOCAL} -I m4 ${ACLOCAL_FLAGS}
     ${AUTOCONF} --force
