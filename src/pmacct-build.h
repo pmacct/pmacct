pmacct/pmacct

commit b6c37df3a6bf5e7c8da63c719dc4c3ee7044fc1f

* BGP, BMP and Streming Telemetry daemons: added writer_id field when writing to Kafka and/or RabbitMQ. The field reports the configured core_proc_name.

src/pmacct-build.h
@@ -1,1 +1,1 @@
-#define PMACCT_BUILD	"20161114-00"
+#define PMACCT_BUILD	"20161115-00"

src/util.c
@@ -2308,6 +2308,25 @@ int write_and_free_json_kafka(void *kafka_log, void *obj)
   return ret;
 }
 #endif
+
+void add_core_name_writer_id_json(void *obj)
+{
+  json_t *kv, *json_obj = (json_t *) obj;
+
+  kv = json_pack("{ss}", "writer_id", config.proc_name);
+  json_object_update_missing(json_obj, kv);
+  json_decref(kv);
+}
+
+void add_plugin_name_writer_id_json(void *obj)
+{
+  json_t *kv, *json_obj = (json_t *) obj;
+
+  kv = json_pack("{ss}", "writer_id", config.name);
+  json_object_update_missing(json_obj, kv);
+  json_decref(kv);
+}
+
 #else
 void *compose_json(u_int64_t wtc, u_int64_t wtc_2, u_int8_t flow_type, struct pkt_primitives *pbase,
                   struct pkt_bgp_primitives *pbgp, struct pkt_nat_primitives *pnat, struct pkt_mpls_primitives *pmpls,
@@ -2355,6 +2374,17 @@ int write_and_free_json_kafka(void *kafka_log, void *obj)
 
   return 0;
 }
+
+void add_core_name_writer_id_json(void *obj)
+{
+  if (config.debug) Log(LOG_DEBUG, "DEBUG ( %s/%s ): add_core_name_writer_id_json(): JSON object not created due to missing --enable-jansson\n", config.name, config.type);
+
+}
+
+void add_plugin_name_writer_id_json(void *obj)
+{
+  if (config.debug) Log(LOG_DEBUG, "DEBUG ( %s/%s ): add_plugin_name_writer_id_json(): JSON object not created due to missing --enable-jansson\n", config.name, config.type);
+
 #endif
 
 #ifdef WITH_AVRO

src/bgp/bgp_logdump.c
@@ -197,6 +197,7 @@ int bgp_peer_log_msg(struct bgp_node *route, struct bgp_info *ri, safi_t safi, c
 #ifdef WITH_RABBITMQ
     if ((bms->msglog_amqp_routing_key && etype == BGP_LOGDUMP_ET_LOG) ||
 	(bms->dump_amqp_routing_key && etype == BGP_LOGDUMP_ET_DUMP)) {
+      add_core_name_writer_id_json(obj);
       amqp_ret = write_and_free_json_amqp(peer->log->amqp_host, obj);
       p_amqp_unset_routing_key(peer->log->amqp_host);
     }
@@ -205,6 +206,7 @@ int bgp_peer_log_msg(struct bgp_node *route, struct bgp_info *ri, safi_t safi, c
 #ifdef WITH_KAFKA
     if ((bms->msglog_kafka_topic && etype == BGP_LOGDUMP_ET_LOG) ||
         (bms->dump_kafka_topic && etype == BGP_LOGDUMP_ET_DUMP)) {
+      add_core_name_writer_id_json(obj);
       kafka_ret = write_and_free_json_kafka(peer->log->kafka_host, obj);
       p_kafka_unset_topic(peer->log->kafka_host);
     }
@@ -313,6 +315,7 @@ int bgp_peer_log_init(struct bgp_peer *peer, int output, int type)
 
 #ifdef WITH_RABBITMQ
       if (bms->msglog_amqp_routing_key) {
+	add_core_name_writer_id_json(obj);
 	amqp_ret = write_and_free_json_amqp(peer->log->amqp_host, obj); 
 	p_amqp_unset_routing_key(peer->log->amqp_host);
       }
@@ -320,6 +323,7 @@ int bgp_peer_log_init(struct bgp_peer *peer, int output, int type)
 
 #ifdef WITH_KAFKA
       if (bms->msglog_kafka_topic) {
+	add_core_name_writer_id_json(obj);
         kafka_ret = write_and_free_json_kafka(peer->log->kafka_host, obj);
         p_kafka_unset_topic(peer->log->kafka_host);
       }
@@ -387,6 +391,7 @@ int bgp_peer_log_close(struct bgp_peer *peer, int output, int type)
 
 #ifdef WITH_RABBITMQ
     if (bms->msglog_amqp_routing_key) {
+      add_core_name_writer_id_json(obj);
       amqp_ret = write_and_free_json_amqp(amqp_log_ptr, obj);
       p_amqp_unset_routing_key(amqp_log_ptr);
     }
@@ -394,6 +399,7 @@ int bgp_peer_log_close(struct bgp_peer *peer, int output, int type)
 
 #ifdef WITH_KAFKA
     if (bms->msglog_kafka_topic) {
+      add_core_name_writer_id_json(obj);
       kafka_ret = write_and_free_json_kafka(kafka_log_ptr, obj);
       p_kafka_unset_topic(kafka_log_ptr);
     }
@@ -516,6 +522,7 @@ int bgp_peer_dump_init(struct bgp_peer *peer, int output, int type)
 
 #ifdef WITH_RABBITMQ
     if (bms->dump_amqp_routing_key) {
+      add_core_name_writer_id_json(obj);
       amqp_ret = write_and_free_json_amqp(peer->log->amqp_host, obj);
       p_amqp_unset_routing_key(peer->log->amqp_host);
     }
@@ -523,6 +530,7 @@ int bgp_peer_dump_init(struct bgp_peer *peer, int output, int type)
 
 #ifdef WITH_KAFKA
     if (bms->dump_kafka_topic) {
+      add_core_name_writer_id_json(obj);
       kafka_ret = write_and_free_json_kafka(peer->log->kafka_host, obj);
       p_kafka_unset_topic(peer->log->kafka_host);
     }
@@ -584,6 +592,7 @@ int bgp_peer_dump_close(struct bgp_peer *peer, struct bgp_dump_stats *bds, int o
 
 #ifdef WITH_RABBITMQ
     if (bms->dump_amqp_routing_key) {
+      add_core_name_writer_id_json(obj);
       amqp_ret = write_and_free_json_amqp(peer->log->amqp_host, obj);
       p_amqp_unset_routing_key(peer->log->amqp_host);
     }
@@ -591,6 +600,7 @@ int bgp_peer_dump_close(struct bgp_peer *peer, struct bgp_dump_stats *bds, int o
 
 #ifdef WITH_KAFKA
     if (bms->dump_kafka_topic) {
+      add_core_name_writer_id_json(obj);
       kafka_ret = write_and_free_json_kafka(peer->log->kafka_host, obj);
       p_kafka_unset_topic(peer->log->kafka_host);
     }

src/bmp/bmp_logdump.c
@@ -118,6 +118,7 @@ int bmp_log_msg(struct bgp_peer *peer, struct bmp_data *bdata, void *log_data, u
 #ifdef WITH_RABBITMQ
     if ((config.nfacctd_bmp_msglog_amqp_routing_key && etype == BGP_LOGDUMP_ET_LOG) ||
 	(config.bmp_dump_amqp_routing_key && etype == BGP_LOGDUMP_ET_DUMP)) {
+      add_core_name_writer_id_json(obj);
       amqp_ret = write_and_free_json_amqp(peer->log->amqp_host, obj);
       p_amqp_unset_routing_key(peer->log->amqp_host);
     }
@@ -126,6 +127,7 @@ int bmp_log_msg(struct bgp_peer *peer, struct bmp_data *bdata, void *log_data, u
 #ifdef WITH_KAFKA
     if ((config.nfacctd_bmp_msglog_kafka_topic && etype == BGP_LOGDUMP_ET_LOG) ||
         (config.bmp_dump_kafka_topic && etype == BGP_LOGDUMP_ET_DUMP)) {
+      add_core_name_writer_id_json(obj);
       kafka_ret = write_and_free_json_kafka(peer->log->kafka_host, obj);
       p_kafka_unset_topic(peer->log->kafka_host);
     }

src/telemetry/telemetry_logdump.c
@@ -135,6 +135,7 @@ int telemetry_log_msg(telemetry_peer *peer, struct telemetry_data *t_data, void 
 #ifdef WITH_RABBITMQ
     if ((config.telemetry_msglog_amqp_routing_key && etype == TELEMETRY_LOGDUMP_ET_LOG) ||
         (config.telemetry_dump_amqp_routing_key && etype == TELEMETRY_LOGDUMP_ET_DUMP)) {
+      add_core_name_writer_id_json(obj);
       amqp_ret = write_and_free_json_amqp(peer->log->amqp_host, obj);
       p_amqp_unset_routing_key(peer->log->amqp_host);
     }
@@ -143,6 +144,7 @@ int telemetry_log_msg(telemetry_peer *peer, struct telemetry_data *t_data, void 
 #ifdef WITH_KAFKA
     if ((config.telemetry_msglog_kafka_topic && etype == TELEMETRY_LOGDUMP_ET_LOG) ||
         (config.telemetry_dump_kafka_topic && etype == TELEMETRY_LOGDUMP_ET_DUMP)) {
+      add_core_name_writer_id_json(obj);
       kafka_ret = write_and_free_json_kafka(peer->log->kafka_host, obj);
       p_kafka_unset_topic(peer->log->kafka_host);
     }

src/util.h
@@ -117,6 +117,8 @@ EXT void *compose_purge_init_json(pid_t);
 EXT void *compose_purge_close_json(pid_t, int, int, int);
 EXT int write_and_free_json_amqp(void *, void *);
 EXT int write_and_free_json_kafka(void *, void *);
+EXT void add_core_name_writer_id_json(void *);
+EXT void add_plugin_name_writer_id_json(void *);
 
 #ifdef WITH_AVRO
 EXT avro_schema_t build_avro_schema(u_int64_t wtc, u_int64_t wtc_2);

commit 23a9f07abf52c031e2a6e0aa77d09633e67e1ae0

* fix, util.c: compiling issue introduced by commit b6c37df3a6bf5e7c8da63c719dc4c3ee7044fc1f

src/pmacct-build.h
@@ -1,1 +1,1 @@
-#define PMACCT_BUILD	"20161116-00"
+#define PMACCT_BUILD	"20161116-01"

src/util.c
@@ -2326,7 +2326,6 @@ void add_plugin_name_writer_id_json(void *obj)
   json_object_update_missing(json_obj, kv);
   json_decref(kv);
 }
-
 #else
 void *compose_json(u_int64_t wtc, u_int64_t wtc_2, u_int8_t flow_type, struct pkt_primitives *pbase,
                   struct pkt_bgp_primitives *pbgp, struct pkt_nat_primitives *pnat, struct pkt_mpls_primitives *pmpls,
@@ -2378,13 +2377,12 @@ int write_and_free_json_kafka(void *kafka_log, void *obj)
 void add_core_name_writer_id_json(void *obj)
 {
   if (config.debug) Log(LOG_DEBUG, "DEBUG ( %s/%s ): add_core_name_writer_id_json(): JSON object not created due to missing --enable-jansson\n", config.name, config.type);
-
 }
 
 void add_plugin_name_writer_id_json(void *obj)
 {
   if (config.debug) Log(LOG_DEBUG, "DEBUG ( %s/%s ): add_plugin_name_writer_id_json(): JSON object not created due to missing --enable-jansson\n", config.name, config.type);
-
+}
 #endif
 
 #ifdef WITH_AVRO