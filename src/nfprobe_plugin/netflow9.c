pmacct/pmacct

commit 6646cfc2bdd2275677763f6acf8a0bf60bacd5c9

* Introducing classification support via nDPI library (29)

src/nfprobe_plugin/netflow9.c
@@ -1,6 +1,6 @@
 /*
     pmacct (Promiscuous mode IP Accounting package)
-    pmacct is Copyright (C) 2003-2014 by Paolo Lucente
+    pmacct is Copyright (C) 2003-2017 by Paolo Lucente
 */
 
 /*
@@ -457,6 +457,18 @@ flow_to_flowset_class_handler(char *flowset, const struct FLOW *flow, int idx, i
 
   return 0;
 }
+
+#if defined (WITH_NDPI)
+static int
+flow_to_flowset_ndpi_class_handler(char *flowset, const struct FLOW *flow, int idx, int size)
+{
+  u_int32_t tmp32 = flow->ndpi_class.app_protocol;
+
+  memcpy(flowset, &tmp32, size);
+
+  return 0;
+}
+#endif
 
 static int
 flow_to_flowset_tag_handler(char *flowset, const struct FLOW *flow, int idx, int size)
@@ -1067,6 +1079,19 @@ nf9_init_template(void)
           v4_int_template_out.r[rcount].length = 4;
           rcount++;
         }
+#if defined (WITH_NDPI)
+	if (config.nfprobe_what_to_count_2 & COUNT_NDPI_CLASS) { 
+	  v4_template.r[rcount].type = htons(NF9_FLOW_APPLICATION_ID);
+	  v4_template.r[rcount].length = htons(4);
+	  v4_int_template.r[rcount].handler = flow_to_flowset_ndpi_class_handler;
+	  v4_int_template.r[rcount].length = 4;
+	  v4_template_out.r[rcount].type = htons(NF9_FLOW_APPLICATION_ID);
+	  v4_template_out.r[rcount].length = htons(4);
+	  v4_int_template_out.r[rcount].handler = flow_to_flowset_ndpi_class_handler;
+	  v4_int_template_out.r[rcount].length = 4;
+	  rcount++;
+	}
+#endif
 
         /* all custom primitives printed here: must be kept last since
 	   only the first field will also have a corresponding handler */
@@ -1449,7 +1474,7 @@ nf9_init_template(void)
           v6_int_template_out.r[rcount].length = 1;
           rcount++;
         }
-        if (config.nfprobe_what_to_count & COUNT_CLASS) {
+        if (config.nfprobe_what_to_count & COUNT_CLASS) { 
           v6_template.r[rcount].type = htons(NF9_FLOW_APPLICATION_ID);
           v6_template.r[rcount].length = htons(4);
           v6_int_template.r[rcount].handler = flow_to_flowset_class_handler;
@@ -1460,6 +1485,19 @@ nf9_init_template(void)
           v6_int_template_out.r[rcount].length = 4;
           rcount++;
         }
+#if defined (WITH_NDPI)
+	if (config.nfprobe_what_to_count_2 & COUNT_NDPI_CLASS) { 
+	  v6_template.r[rcount].type = htons(NF9_FLOW_APPLICATION_ID);
+	  v6_template.r[rcount].length = htons(4);
+	  v6_int_template.r[rcount].handler = flow_to_flowset_ndpi_class_handler;
+	  v6_int_template.r[rcount].length = 4;
+	  v6_template_out.r[rcount].type = htons(NF9_FLOW_APPLICATION_ID);
+	  v6_template_out.r[rcount].length = htons(4);
+	  v6_int_template_out.r[rcount].handler = flow_to_flowset_ndpi_class_handler;
+	  v6_int_template_out.r[rcount].length = 4;
+	  rcount++;
+	}
+#endif
 
         /* all custom primitives printed here: must be kept last since
            only the first field will also have a corresponding handler */
@@ -2093,7 +2131,11 @@ send_netflow_v9(struct FLOW **flows, int num_flows, int nfsock,
 			  send_options = TRUE;
 			  send_sampling_option = TRUE;
 			}
-			if (config.nfprobe_what_to_count & COUNT_CLASS && num_class) {
+			if (((config.nfprobe_what_to_count & COUNT_CLASS) ||
+#if defined (WITH_NDPI) 
+			    (config.nfprobe_what_to_count_2 & COUNT_NDPI_CLASS)
+#endif
+			    ) && num_class > 0) {
                           memcpy(packet + offset, &class_option_template, class_option_template.tot_len);
                           offset += class_option_template.tot_len;
                           flows++;

src/nfprobe_plugin/nfprobe_plugin.c
@@ -455,6 +455,7 @@ ipv4_to_flowrec(struct FLOW *flow, struct primitives_ptrs *prim_ptrs, int *isfra
   flow->packets[ndx] = data->pkt_num;
   flow->flows[ndx] = data->flo_num;
   flow->class = p->class;
+  memcpy(&flow->ndpi_class, &p->ndpi_class, sizeof(pm_class2_t));
   flow->tag[ndx] = p->tag;
   flow->tag2[ndx] = p->tag2;
 
@@ -518,6 +519,7 @@ ipv6_to_flowrec(struct FLOW *flow, struct primitives_ptrs *prim_ptrs, int *isfra
   flow->packets[ndx] = data->pkt_num; 
   flow->flows[ndx] = data->flo_num;
   flow->class = p->class;
+  memcpy(&flow->ndpi_class, &p->ndpi_class, sizeof(pm_class2_t));
   flow->tag[ndx] = p->tag;
   flow->tag2[ndx] = p->tag2;
 
@@ -756,6 +758,8 @@ process_packet(struct FLOWTRACK *ft, struct primitives_ptrs *prim_ptrs, const st
 #endif
     }
     if (!flow->class) flow->class = tmp.class;
+    if (!flow->ndpi_class.app_protocol)
+      memcpy(&flow->ndpi_class, &tmp.ndpi_class, sizeof(pm_class2_t));
     if (!flow->tag[0]) flow->tag[0] = tmp.tag[0];
     if (!flow->tag[1]) flow->tag[1] = tmp.tag[1];
     if (!flow->tag2[0]) flow->tag2[0] = tmp.tag2[0];

src/nfprobe_plugin/nfprobe_plugin.h
@@ -180,6 +180,7 @@ struct FLOW {
 
 	/* classification stuff */
 	pm_class_t class;			/* Classification internal ID */
+	pm_class2_t ndpi_class;			/* nDPI classification internal ID */
 	pm_id_t tag[2];				/* Tag */
 	pm_id_t tag2[2];			/* Tag2 */
 

src/pmacct-build.h
@@ -1,1 +1,1 @@
-#define PMACCT_BUILD	"20170703-00"
+#define PMACCT_BUILD	"20170703-01"

src/classifier.c
@@ -585,6 +585,26 @@ pm_class_t pmct_register(struct pkt_classifier *css)
   else return 0;
 }
 
+/* same as pmct_register but without the index decrement */
+pm_class_t pmct_ndpi_register(struct pkt_classifier *css)
+{
+  int max = pmct_get_num_entries();
+
+  if (!css) return 0;
+
+  /* let's check that a) a valid class ID has been supplied, b) the class ID
+     is still available. If this is the case, let's proceed with this entry,
+     otherwise we will switch to a default behaviour. */
+
+  if (!strcmp(css->protocol, "")) return 0;
+
+  if (css->id <= max && !class[css->id].id) {
+    memcpy(&class[css->id], css, sizeof(struct pkt_classifier));
+    return css->id;
+  }
+  else return 0;
+}
+
 void pmct_unregister(pm_class_t id)
 {
   int max = pmct_get_num_entries();

src/classifier.h
@@ -78,6 +78,7 @@ EXT void clear_context_chain(struct ip_flow_common *, unsigned int);
 EXT void prepare_classifier_data(struct pkt_classifier_data *, struct ip_flow_common *, unsigned int, struct packet_ptrs *);
 
 EXT pm_class_t pmct_register(struct pkt_classifier *);
+EXT pm_class_t pmct_ndpi_register(struct pkt_classifier *);
 EXT void pmct_unregister(pm_class_t);
 EXT pm_class_t pmct_find_first_free();
 EXT pm_class_t pmct_find_last_free();

src/ndpi/ndpi_util.c
@@ -560,9 +560,9 @@ void ndpi_export_proto_to_class(struct ndpi_workflow *workflow)
 
   for (idx = 0; idx < (int) workflow->ndpi_struct->ndpi_num_supported_protocols; idx++) {
     memset(&css, 0, sizeof(css));
-    css.id = idx + 1; /* id are >= 1 */
+    css.id = workflow->ndpi_struct->proto_defaults[idx].protoId;
     strncpy(css.protocol, workflow->ndpi_struct->proto_defaults[idx].protoName, MAX_PROTOCOL_LEN);
-    pmct_register(&css);
+    pmct_ndpi_register(&css);
   }
 }
 #endif

src/pmacctd.c
@@ -466,9 +466,13 @@ int main(int argc,char **argv, char **envp)
 	  list->cfg.what_to_count |= COUNT_DST_AS;
 	  list->cfg.what_to_count |= COUNT_PEER_DST_IP;
 	}
-	if ((list->cfg.nfprobe_version == 9 || list->cfg.nfprobe_version == 10) && list->cfg.classifiers_path) {
-	  list->cfg.what_to_count |= COUNT_CLASS;
-	  config.handle_flows = TRUE;
+	if (list->cfg.nfprobe_version == 9 || list->cfg.nfprobe_version == 10) {
+	  if (list->cfg.classifiers_path) {
+	    list->cfg.what_to_count |= COUNT_CLASS;
+	    config.handle_flows = TRUE;
+	  }
+	  if (list->cfg.nfprobe_what_to_count_2 & COUNT_NDPI_CLASS)
+	    list->cfg.what_to_count_2 |= COUNT_NDPI_CLASS;
 	}
 	if (list->cfg.pre_tag_map) {
 	  list->cfg.what_to_count |= COUNT_TAG;
@@ -623,7 +627,8 @@ int main(int argc,char **argv, char **envp)
       }
 
       /* applies to all plugins */
-      if (list->cfg.what_to_count_2 & COUNT_NDPI_CLASS) {
+      if ((list->cfg.what_to_count_2 & COUNT_NDPI_CLASS) ||
+	  (list->cfg.nfprobe_what_to_count_2 & COUNT_NDPI_CLASS)) {
 	config.handle_fragments = TRUE;
 	config.classifier_ndpi = TRUE;
       }

commit 2fb35c2b9940a0d22778e333c82bb56c4a191cd8

* fix, nfprobe: compile issue introduced with commit 6646cfc2bdd2275677763f6acf8a0bf60bacd5c9

src/nfprobe_plugin/netflow9.c
@@ -2131,9 +2131,9 @@ send_netflow_v9(struct FLOW **flows, int num_flows, int nfsock,
 			  send_options = TRUE;
 			  send_sampling_option = TRUE;
 			}
-			if (((config.nfprobe_what_to_count & COUNT_CLASS) ||
+			if (((config.nfprobe_what_to_count & COUNT_CLASS)
 #if defined (WITH_NDPI) 
-			    (config.nfprobe_what_to_count_2 & COUNT_NDPI_CLASS)
+			    || (config.nfprobe_what_to_count_2 & COUNT_NDPI_CLASS)
 #endif
 			    ) && num_class > 0) {
                           memcpy(packet + offset, &class_option_template, class_option_template.tot_len);

src/nfprobe_plugin/nfprobe_plugin.c
@@ -455,7 +455,9 @@ ipv4_to_flowrec(struct FLOW *flow, struct primitives_ptrs *prim_ptrs, int *isfra
   flow->packets[ndx] = data->pkt_num;
   flow->flows[ndx] = data->flo_num;
   flow->class = p->class;
+#if defined (WITH_NDPI)
   memcpy(&flow->ndpi_class, &p->ndpi_class, sizeof(pm_class2_t));
+#endif
   flow->tag[ndx] = p->tag;
   flow->tag2[ndx] = p->tag2;
 
@@ -519,7 +521,9 @@ ipv6_to_flowrec(struct FLOW *flow, struct primitives_ptrs *prim_ptrs, int *isfra
   flow->packets[ndx] = data->pkt_num; 
   flow->flows[ndx] = data->flo_num;
   flow->class = p->class;
+#if defined (WITH_NDPI)
   memcpy(&flow->ndpi_class, &p->ndpi_class, sizeof(pm_class2_t));
+#endif
   flow->tag[ndx] = p->tag;
   flow->tag2[ndx] = p->tag2;
 
@@ -758,8 +762,10 @@ process_packet(struct FLOWTRACK *ft, struct primitives_ptrs *prim_ptrs, const st
 #endif
     }
     if (!flow->class) flow->class = tmp.class;
+#if defined (WITH_NDPI)
     if (!flow->ndpi_class.app_protocol)
       memcpy(&flow->ndpi_class, &tmp.ndpi_class, sizeof(pm_class2_t));
+#endif
     if (!flow->tag[0]) flow->tag[0] = tmp.tag[0];
     if (!flow->tag[1]) flow->tag[1] = tmp.tag[1];
     if (!flow->tag2[0]) flow->tag2[0] = tmp.tag2[0];

src/nfprobe_plugin/nfprobe_plugin.h
@@ -180,7 +180,9 @@ struct FLOW {
 
 	/* classification stuff */
 	pm_class_t class;			/* Classification internal ID */
+#if defined (WITH_NDPI)
 	pm_class2_t ndpi_class;			/* nDPI classification internal ID */
+#endif
 	pm_id_t tag[2];				/* Tag */
 	pm_id_t tag2[2];			/* Tag2 */
 

src/pmacct-build.h
@@ -1,1 +1,1 @@
-#define PMACCT_BUILD	"20170703-01"
+#define PMACCT_BUILD	"20170703-02"