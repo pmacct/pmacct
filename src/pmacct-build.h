pmacct/pmacct

commit 14aa50b50079299f83630e32ea6ad1efb7107294

* Kafka, AMQP plugins: streamlined from P_handle_table_dyn_strings() to handle_dynname_internal_strings(). * Kafka, AMQP, print plugins: optimized use of handle_dynname_internal_strings().

src/pmacct-build.h
@@ -1,1 +1,1 @@
-#define PMACCT_BUILD	"20171023-01"
+#define PMACCT_BUILD	"20171024-00"

src/print_plugin.c
@@ -347,7 +347,7 @@ void P_cache_purge(struct chained_cache *queue[], int index, int safe_action)
   FILE *f = NULL, *lockf = NULL;
   int j, stop, is_event = FALSE, qn = 0, go_to_pending, saved_index = index, file_to_be_created;
   time_t start, duration;
-  char tmpbuf[LONGLONGSRVBUFLEN], current_table[SRVBUFLEN], elem_table[SRVBUFLEN];
+  char tmpbuf[SRVBUFLEN], current_table[SRVBUFLEN], elem_table[SRVBUFLEN];
   struct primitives_ptrs prim_ptrs, elem_prim_ptrs;
   struct pkt_data dummy_data, elem_dummy_data;
   pid_t writer_pid = getpid();
@@ -398,16 +398,13 @@ void P_cache_purge(struct chained_cache *queue[], int index, int safe_action)
   if (config.sql_table) {
     time_t stamp = 0;
 
-    strlcpy(current_table, config.sql_table, SRVBUFLEN);
-
     if (dyn_table) {
       stamp = queue[0]->basetime.tv_sec;
-
       prim_ptrs.data = &dummy_data;
       primptrs_set_all_from_chained_cache(&prim_ptrs, queue[0]);
 
-      handle_dynname_internal_strings_same(current_table, LONGSRVBUFLEN, tmpbuf, &prim_ptrs, DYN_STR_PRINT_FILE);
-      pm_strftime_same(current_table, LONGSRVBUFLEN, tmpbuf, &stamp, config.timestamps_utc);
+      handle_dynname_internal_strings(current_table, SRVBUFLEN, config.sql_table, &prim_ptrs, DYN_STR_PRINT_FILE);
+      pm_strftime_same(current_table, SRVBUFLEN, tmpbuf, &stamp, config.timestamps_utc);
     }
 
     if (config.print_output & PRINT_OUTPUT_AVRO) {
@@ -504,14 +501,12 @@ void P_cache_purge(struct chained_cache *queue[], int index, int safe_action)
     if (dyn_table && (!dyn_table_time_only || !config.nfacctd_time_new)) {
       time_t stamp = 0;
 
-      memset(tmpbuf, 0, LONGLONGSRVBUFLEN); // XXX: pedantic?
       stamp = queue[j]->basetime.tv_sec;
-      strlcpy(elem_table, config.sql_table, SRVBUFLEN);
-
       elem_prim_ptrs.data = &elem_dummy_data;
       primptrs_set_all_from_chained_cache(&elem_prim_ptrs, queue[j]);
-      handle_dynname_internal_strings_same(elem_table, LONGSRVBUFLEN, tmpbuf, &elem_prim_ptrs, DYN_STR_PRINT_FILE);
-      pm_strftime_same(elem_table, LONGSRVBUFLEN, tmpbuf, &stamp, config.timestamps_utc);
+
+      handle_dynname_internal_strings(elem_table, SRVBUFLEN, config.sql_table, &elem_prim_ptrs, DYN_STR_PRINT_FILE);
+      pm_strftime_same(elem_table, SRVBUFLEN, tmpbuf, &stamp, config.timestamps_utc);
 
       if (strncmp(current_table, elem_table, SRVBUFLEN)) {
         pending_queries_queue[pqq_ptr] = queue[j];
@@ -1297,8 +1292,7 @@ void P_cache_purge(struct chained_cache *queue[], int index, int safe_action)
 
     if (config.print_latest_file) {
       if (!safe_action) {
-        memset(tmpbuf, 0, LONGLONGSRVBUFLEN);
-        handle_dynname_internal_strings(tmpbuf, LONGSRVBUFLEN, config.print_latest_file, &prim_ptrs, DYN_STR_PRINT_FILE);
+        handle_dynname_internal_strings(tmpbuf, SRVBUFLEN, config.print_latest_file, &prim_ptrs, DYN_STR_PRINT_FILE);
         link_latest_output_file(tmpbuf, current_table);
       }
     }

src/amqp_plugin.c
@@ -345,9 +345,12 @@ void amqp_cache_purge(struct chained_cache *queue[], int index, int safe_action)
   char *empty_pcust = NULL;
   char src_mac[18], dst_mac[18], src_host[INET6_ADDRSTRLEN], dst_host[INET6_ADDRSTRLEN], ip_address[INET6_ADDRSTRLEN];
   char rd_str[SRVBUFLEN], misc_str[SRVBUFLEN], dyn_amqp_routing_key[SRVBUFLEN], *orig_amqp_routing_key = NULL;
+  char tmpbuf[SRVBUFLEN];
   int i, j, stop, batch_idx, is_routing_key_dyn = FALSE, qn = 0, ret, saved_index = index;
   int mv_num = 0, mv_num_save = 0;
   time_t start, duration;
+  struct primitives_ptrs prim_ptrs;
+  struct pkt_data dummy_data;
   pid_t writer_pid = getpid();
 
   char *json_buf = NULL;
@@ -566,7 +569,10 @@ void amqp_cache_purge(struct chained_cache *queue[], int index, int safe_action)
 
       if (json_str) {
         if (is_routing_key_dyn) {
-          P_handle_table_dyn_strings(dyn_amqp_routing_key, SRVBUFLEN, orig_amqp_routing_key, queue[j]);
+	  prim_ptrs.data = &dummy_data;
+	  primptrs_set_all_from_chained_cache(&prim_ptrs, queue[j]);
+
+	  handle_dynname_internal_strings(dyn_amqp_routing_key, SRVBUFLEN, orig_amqp_routing_key, &prim_ptrs, DYN_STR_RABBITMQ_RK);
           p_amqp_set_routing_key(&amqpp_amqp_host, dyn_amqp_routing_key);
         }
 
@@ -603,7 +609,10 @@ void amqp_cache_purge(struct chained_cache *queue[], int index, int safe_action)
 #ifdef WITH_AVRO
       if (!config.sql_multi_values || (mv_num >= config.sql_multi_values) || avro_buffer_full) {
         if (is_routing_key_dyn) {
-          P_handle_table_dyn_strings(dyn_amqp_routing_key, SRVBUFLEN, orig_amqp_routing_key, queue[j]);
+          prim_ptrs.data = &dummy_data;
+          primptrs_set_all_from_chained_cache(&prim_ptrs, queue[j]);
+
+          handle_dynname_internal_strings(dyn_amqp_routing_key, SRVBUFLEN, orig_amqp_routing_key, &prim_ptrs, DYN_STR_RABBITMQ_RK);
           p_amqp_set_routing_key(&amqpp_amqp_host, dyn_amqp_routing_key);
         }
 

src/kafka_plugin.c
@@ -500,9 +500,8 @@ void kafka_cache_purge(struct chained_cache *queue[], int index, int safe_action
     if (dyn_partition_key) {
       prim_ptrs.data = &dummy_data;
       primptrs_set_all_from_chained_cache(&prim_ptrs, queue[j]);
-      memset(tmpbuf, 0, SRVBUFLEN);
-      strlcpy(elem_part_key, config.kafka_partition_key, SRVBUFLEN);
-      handle_dynname_internal_strings_same(tmpbuf, SRVBUFLEN, elem_part_key, &prim_ptrs, DYN_STR_KAFKA_PART);
+
+      handle_dynname_internal_strings(elem_part_key, SRVBUFLEN, config.kafka_partition_key, &prim_ptrs, DYN_STR_KAFKA_PART);
       p_kafka_set_key(&kafkap_kafka_host, elem_part_key, strlen(elem_part_key));
     }
 
@@ -585,7 +584,10 @@ void kafka_cache_purge(struct chained_cache *queue[], int index, int safe_action
 
       if (json_str) {
         if (is_topic_dyn) {
-          P_handle_table_dyn_strings(dyn_kafka_topic, SRVBUFLEN, orig_kafka_topic, queue[j]);
+          prim_ptrs.data = &dummy_data;
+          primptrs_set_all_from_chained_cache(&prim_ptrs, queue[j]);
+
+	  handle_dynname_internal_strings(dyn_kafka_topic, SRVBUFLEN, orig_kafka_topic, &prim_ptrs, DYN_STR_KAFKA_TOPIC);
           p_kafka_set_topic(&kafkap_kafka_host, dyn_kafka_topic);
         }
 
@@ -622,7 +624,10 @@ void kafka_cache_purge(struct chained_cache *queue[], int index, int safe_action
 #ifdef WITH_AVRO
       if (!config.sql_multi_values || (mv_num >= config.sql_multi_values) || avro_buffer_full) {
         if (is_topic_dyn) {
-          P_handle_table_dyn_strings(dyn_kafka_topic, SRVBUFLEN, orig_kafka_topic, queue[j]);
+	  prim_ptrs.data = &dummy_data;
+	  primptrs_set_all_from_chained_cache(&prim_ptrs, queue[j]);
+
+	  handle_dynname_internal_strings(dyn_kafka_topic, SRVBUFLEN, orig_kafka_topic, &prim_ptrs, DYN_STR_KAFKA_TOPIC);
           p_kafka_set_topic(&kafkap_kafka_host, dyn_kafka_topic);
         }
 

src/mongodb_plugin.c
@@ -305,7 +305,7 @@ void MongoDB_cache_purge(struct chained_cache *queue[], int index, int safe_acti
   struct pkt_tunnel_primitives empty_ptun;
   char *empty_pcust = NULL;
   char src_mac[18], dst_mac[18], src_host[INET6_ADDRSTRLEN], dst_host[INET6_ADDRSTRLEN], ip_address[INET6_ADDRSTRLEN];
-  char rd_str[SRVBUFLEN], misc_str[SRVBUFLEN], tmpbuf[LONGLONGSRVBUFLEN], mongo_database[SRVBUFLEN];
+  char rd_str[SRVBUFLEN], misc_str[SRVBUFLEN], tmpbuf[SRVBUFLEN], mongo_database[SRVBUFLEN];
   char *str_ptr, *as_path, *bgp_comm, default_table[] = "test.acct", ndpi_class[SUPERSHORTBUFLEN];
   char default_user[] = "pmacct", default_passwd[] = "arealsmartpwd";
   int qn = 0, i, j, stop, db_status, batch_idx, go_to_pending, saved_index = index;
@@ -414,13 +414,11 @@ void MongoDB_cache_purge(struct chained_cache *queue[], int index, int safe_acti
 
   if (dyn_table) {
     stamp = queue[0]->basetime.tv_sec;
-    strlcpy(current_table, config.sql_table, SRVBUFLEN);
-
     prim_ptrs.data = &dummy_data;
     primptrs_set_all_from_chained_cache(&prim_ptrs, queue[0]);
 
-    handle_dynname_internal_strings_same(current_table, LONGSRVBUFLEN, tmpbuf, &prim_ptrs, DYN_STR_MONGODB_TABLE);
-    pm_strftime_same(current_table, LONGSRVBUFLEN, tmpbuf, &stamp, config.timestamps_utc);
+    handle_dynname_internal_strings(current_table, SRVBUFLEN, config.sql_table, &prim_ptrs, DYN_STR_MONGODB_TABLE);
+    pm_strftime_same(current_table, SRVBUFLEN, tmpbuf, &stamp, config.timestamps_utc);
     if (config.sql_table_schema) MongoDB_create_indexes(&db_conn, tmpbuf);
   }
 
@@ -432,14 +430,12 @@ void MongoDB_cache_purge(struct chained_cache *queue[], int index, int safe_acti
     if (dyn_table && (!dyn_table_time_only || !config.nfacctd_time_new)) {
       time_t stamp = 0;
 
-      memset(tmpbuf, 0, LONGLONGSRVBUFLEN); // XXX: pedantic?
       stamp = queue[j]->basetime.tv_sec;
-      strlcpy(elem_table, config.sql_table, SRVBUFLEN);
-
       prim_ptrs.data = &dummy_data;
       primptrs_set_all_from_chained_cache(&prim_ptrs, queue[j]);
-      handle_dynname_internal_strings_same(elem_table, LONGSRVBUFLEN, tmpbuf, &prim_ptrs, DYN_STR_MONGODB_TABLE);
-      pm_strftime_same(elem_table, LONGSRVBUFLEN, tmpbuf, &stamp, config.timestamps_utc);
+
+      handle_dynname_internal_strings(elem_table, SRVBUFLEN, config.sql_table, &prim_ptrs, DYN_STR_MONGODB_TABLE);
+      pm_strftime_same(elem_table, SRVBUFLEN, tmpbuf, &stamp, config.timestamps_utc);
 
       if (strncmp(current_table, elem_table, SRVBUFLEN)) {
         pending_queries_queue[pqq_ptr] = queue[j];

src/sql_common.c
@@ -3221,7 +3221,7 @@ void sql_create_table(struct DBdesc *db, time_t *basetime, struct primitives_ptr
 
   ret = read_SQLquery_from_file(config.sql_table_schema, buf, LONGSRVBUFLEN);
   if (ret) {
-    handle_dynname_internal_strings_same(tmpbuf, LONGSRVBUFLEN, buf, prim_ptrs, DYN_STR_SQL_TABLE);
+    handle_dynname_internal_strings_same(buf, LONGSRVBUFLEN, tmpbuf, prim_ptrs, DYN_STR_SQL_TABLE);
     pm_strftime_same(buf, LONGSRVBUFLEN, tmpbuf, basetime, config.timestamps_utc);
     (*sqlfunc_cbr.create_table)(db, buf);
   }

commit 792e6fe22d9563d184ab33e967907b834972751e

* fix, print plugin: commit 14aa50b50079299f83630e32ea6ad1efb7107294 broke the use of non-dynamic files.

src/pmacct-build.h
@@ -1,1 +1,1 @@
-#define PMACCT_BUILD	"20171102-00"
+#define PMACCT_BUILD	"20171103-00"

src/print_plugin.c
@@ -395,6 +395,7 @@ void P_cache_purge(struct chained_cache *queue[], int index, int safe_action)
       handle_dynname_internal_strings(current_table, SRVBUFLEN, config.sql_table, &prim_ptrs, DYN_STR_PRINT_FILE);
       pm_strftime_same(current_table, SRVBUFLEN, tmpbuf, &stamp, config.timestamps_utc);
     }
+    else strlcpy(current_table, config.sql_table, SRVBUFLEN);
 
     if (config.print_output & PRINT_OUTPUT_AVRO) {
       int file_is_empty, ret;