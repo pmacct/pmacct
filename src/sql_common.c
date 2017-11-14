6d5e6925c7d3f60d08bef034c923546a77bf99ec
Paolo Lucente <pl+github@pmacct.net>
2017-11-14 10:33:01 +0000

    * fix, sql_common.c: increased read_SQLquery_from_file() buffer size (2). To address issue #169.

--- src/sql_common.c
+++ src/sql_common.c
@@ -3199,7 +3199,7 @@ void sql_create_table(struct DBdesc *db, time_t *basetime, struct primitives_ptr
 
   ret = read_SQLquery_from_file(config.sql_table_schema, buf, LARGEBUFLEN);
   if (ret) {
-    handle_dynname_internal_strings_same(buf, LONGSRVBUFLEN, tmpbuf, prim_ptrs, DYN_STR_SQL_TABLE);
+    handle_dynname_internal_strings_same(buf, LARGEBUFLEN, tmpbuf, prim_ptrs, DYN_STR_SQL_TABLE);
     pm_strftime_same(buf, LARGEBUFLEN, tmpbuf, basetime, config.timestamps_utc);
     (*sqlfunc_cbr.create_table)(db, buf);
   }
