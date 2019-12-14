48e14793a70a471a50bea3f145d3d387bc6d3a37
Paolo Lucente <pl+github@pmacct.net>
2019-12-14 15:34:41 +0000

    * fix, zmq_common.c: missing variable initialization in p_zmq_zap_handler() gives troubles with gcc7 compiler optimizations. Thanks to Yuri Lachin ( @yuyutime ) for his support resolving this bug.

--- src/zmq_common.c
+++ src/zmq_common.c
@@ -663,6 +663,8 @@ void p_zmq_zap_handler(void *zh)
   struct p_zmq_sock zmq_sock;
   int ret;
 
+  memset(&zmq_sock, 0, sizeof(zmq_sock));
+
   zmq_sock.obj = zmq_socket(zmq_host->ctx, ZMQ_REP);
   if (!zmq_sock.obj) {
     Log(LOG_ERR, "ERROR ( %s ): zmq_socket() ZAP failed (%s)\nExiting.\n",
