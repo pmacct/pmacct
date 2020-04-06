pmacct/pmacct

commit 4761d677eb8e0df583282fea90d3b77b19b12059

* BMP daemon: introducing support for draft-xu-grow-bmp-route-policy-attr-trace (1)

src/pmacct-build.h
@@ -1,1 +1,1 @@
-#define PMACCT_BUILD	"20200324-02"
+#define PMACCT_BUILD	"20200325-00"

src/bmp/Makefile.am
@@ -3,5 +3,6 @@ AM_CFLAGS = $(PMACCT_CFLAGS)
 noinst_LTLIBRARIES = libpmbmp.la
 libpmbmp_la_SOURCES = bmp.c bmp_logdump.c bmp_msg.c bmp_util.c	\
 	bmp_lookup.c bmp.h bmp_logdump.h bmp_msg.h bmp_util.h	\
-	bmp_lookup.h bmp_tlv.c bmp_tlv.h bmp-globals.c
+	bmp_lookup.h bmp_tlv.c bmp_tlv.h bmp_rpat.c bmp_rpat.h	\
+	bmp-globals.c
 libpmbmp_la_CFLAGS = -I$(srcdir)/.. $(AM_CFLAGS)

src/bmp/bmp.h
@@ -40,7 +40,8 @@
 #define	BMP_MSG_INIT			4
 #define BMP_MSG_TERM			5
 #define BMP_MSG_ROUTE_MIRROR		6
-#define BMP_MSG_TYPE_MAX		6 /* set to the highest BMP_MSG_* value */
+#define BMP_MSG_TMP_RPAT		100
+#define BMP_MSG_TYPE_MAX		100 /* set to the highest BMP_MSG_* value */
 
 static const char __attribute__((unused)) *bmp_msg_types[] = {
   "Route Monitoring",
@@ -288,6 +289,7 @@ struct bmp_data {
 #include "bmp_util.h"
 #include "bmp_lookup.h"
 #include "bmp_tlv.h"
+#include "bmp_rpat.h"
 
 /* prototypes */
 extern void bmp_daemon_wrapper();

src/bmp/bmp_logdump.h
@@ -29,6 +29,7 @@
 #define BMP_LOG_TYPE_PEER_UP	BMP_MSG_PEER_UP
 #define BMP_LOG_TYPE_PEER_DOWN	BMP_MSG_PEER_DOWN
 #define BMP_LOG_TYPE_ROUTE	BMP_MSG_ROUTE_MONITOR
+#define BMP_LOG_TYPE_RPAT	BMP_MSG_TMP_RPAT
 
 #define BMP_LOG_TYPE_LOGINIT    BGP_LOG_TYPE_LOGINIT
 #define BMP_LOG_TYPE_LOGCLOSE	BGP_LOG_TYPE_LOGCLOSE

src/bmp/bmp_msg.c
@@ -54,8 +54,9 @@ u_int32_t bmp_process_packet(char *bmp_packet, u_int32_t len, struct bmp_peer *b
   }
 
   for (msg_start_len = pkt_remaining_len = len; pkt_remaining_len; msg_start_len = pkt_remaining_len) {
-    if (!(bch = (struct bmp_common_hdr *) bmp_get_and_check_length(&bmp_packet_ptr, &pkt_remaining_len, sizeof(struct bmp_common_hdr))))
+    if (!(bch = (struct bmp_common_hdr *) bmp_get_and_check_length(&bmp_packet_ptr, &pkt_remaining_len, sizeof(struct bmp_common_hdr)))) { 
       return msg_start_len;
+    }
 
     if (bch->version != BMP_V3 && bch->version != BMP_V4) {
       Log(LOG_INFO, "INFO ( %s/%s ): [%s] packet discarded: unknown BMP version: %u\n",
@@ -97,12 +98,16 @@ u_int32_t bmp_process_packet(char *bmp_packet, u_int32_t len, struct bmp_peer *b
     case BMP_MSG_ROUTE_MIRROR:
       bmp_process_msg_route_mirror(&bmp_packet_ptr, &msg_len, bmpp);
       break;
+    case BMP_MSG_TMP_RPAT:
+      bmp_process_msg_rpat(&bmp_packet_ptr, &msg_len, bmpp);
+      break;
     default:
       Log(LOG_INFO, "INFO ( %s/%s ): [%s] packet discarded: unknown message type (%u)\n",
 	  config.name, bms->log_str, peer->addr_str, bch->type);
       break;
     }
 
+    printf("CI PASSO: orig_msg_len=%d msg_len=%d pkt_remaining_len=%d\n", orig_msg_len, msg_len, pkt_remaining_len);
     /* sync-up status of pkt_remaining_len to bmp_packet_ptr */
     pkt_remaining_len -= (orig_msg_len - msg_len);
  

src/bmp/bmp_rpat.c
@@ -1,0 +1,202 @@
+/*  
+    pmacct (Promiscuous mode IP Accounting package)
+    pmacct is Copyright (C) 2003-2020 by Paolo Lucente
+*/
+
+/*
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
+
+/* includes */
+#include "pmacct.h"
+#include "addr.h"
+#include "bgp/bgp.h"
+#include "bmp.h"
+
+/* functions */
+void bmp_process_msg_rpat(char **bmp_packet, u_int32_t *len, struct bmp_peer *bmpp)
+{
+  struct bgp_misc_structs *bms;
+  struct bgp_peer *peer;
+  struct bmp_data bdata;
+  struct bmp_rpat_common_hdr *brch;
+  struct bmp_rpat_event_hdr *breh; 
+  struct bmp_log_rpat blrpat;
+  int idx, tecount, telen;
+
+  if (!bmpp) return;
+
+  peer = &bmpp->self;
+  bms = bgp_select_misc_db(peer->type);
+
+  if (!bms) return;
+
+  memset(&bdata, 0, sizeof(bdata));
+  memset(&blrpat, 0, sizeof(blrpat));
+
+  if (!(brch = (struct bmp_rpat_common_hdr *) bmp_get_and_check_length(bmp_packet, len, sizeof(struct bmp_rpat_common_hdr)))) {
+    Log(LOG_INFO, "INFO ( %s/%s ): [%s] [rpat] packet discarded: failed bmp_get_and_check_length() BMP rpat common hdr\n",
+	config.name, bms->log_str, peer->addr_str);
+    return;
+  }
+
+  bmp_rpat_common_hdr_get_v_flag(brch, &bdata.family);
+  bmp_rpat_common_hdr_get_bgp_id(brch, &bdata.bgp_id);
+  // XXX: collect RD info
+  bmp_rpat_common_hdr_get_prefix(brch, &blrpat.prefix, &bdata.family);
+  bmp_rpat_common_hdr_get_prefix_len(brch, &blrpat.prefix_len);
+
+  tecount = brch->events_count;
+  telen = ntohs(brch->events_length);
+
+  for (idx = 0; (idx < tecount) && (telen > 0); idx++) {
+    u_int32_t orig_loc_len, rmn_loc_len;
+    int ret, elen;
+
+    /* TLV vars */
+    struct bmp_tlv_hdr *bth;
+    u_int16_t bmp_tlv_type, bmp_tlv_len;
+    char *bmp_tlv_value;
+    struct pm_list *tlvs = NULL;
+
+    tlvs = bmp_tlv_list_new(NULL, bmp_tlv_list_node_del);
+    if (!tlvs) return;
+
+    if (!(breh = (struct bmp_rpat_event_hdr *) bmp_get_and_check_length(bmp_packet, len, sizeof(struct bmp_rpat_event_hdr)))) {
+      Log(LOG_INFO, "INFO ( %s/%s ): [%s] [rpat] packet discarded: failed bmp_get_and_check_length() BMP rpat event hdr\n",
+	  config.name, bms->log_str, peer->addr_str);
+      bmp_tlv_list_destroy(tlvs);
+      return;
+    }
+
+    elen = rmn_loc_len = ntohs(breh->len);
+    orig_loc_len = rmn_loc_len = (rmn_loc_len - sizeof(struct bmp_rpat_event_hdr));
+    
+    bmp_rpat_event_hdr_get_index(breh, &blrpat.event_idx);
+    bmp_rpat_event_hdr_get_tstamp(breh, &blrpat.tstamp);
+    bmp_rpat_event_hdr_get_path_id(breh, &blrpat.path_id);
+    bmp_rpat_event_hdr_get_afi_safi(breh, &blrpat.afi, &blrpat.safi);
+
+    while (rmn_loc_len) {
+      if (!(bth = (struct bmp_tlv_hdr *) bmp_get_and_check_length(bmp_packet, &rmn_loc_len, sizeof(struct bmp_tlv_hdr)))) {
+	Log(LOG_INFO, "INFO ( %s/%s ): [%s] [rpat] packet discarded: failed bmp_get_and_check_length() BMP TLV hdr\n",
+	    config.name, bms->log_str, peer->addr_str);
+	(*len) -= (orig_loc_len - rmn_loc_len);
+        bmp_tlv_list_destroy(tlvs);
+        return;
+      }
+
+      bmp_tlv_hdr_get_type(bth, &bmp_tlv_type);
+      bmp_tlv_hdr_get_len(bth, &bmp_tlv_len);
+
+      if (!(bmp_tlv_value = bmp_get_and_check_length(bmp_packet, &rmn_loc_len, bmp_tlv_len))) {
+	Log(LOG_INFO, "INFO ( %s/%s ): [%s] [rpat] packet discarded: failed bmp_get_and_check_length() BMP TLV info\n",
+	    config.name, bms->log_str, peer->addr_str);
+	(*len) -= (orig_loc_len - rmn_loc_len);
+	bmp_tlv_list_destroy(tlvs);
+	return;
+      }
+
+      ret = bmp_tlv_list_add(tlvs, 0, bmp_tlv_type, bmp_tlv_len, bmp_tlv_value);
+      if (ret == ERR) {
+	Log(LOG_ERR, "ERROR ( %s/%s ): [%s] [rpat] bmp_tlv_list_add() failed.\n", config.name, bms->log_str, peer->addr_str);
+	exit_gracefully(1);
+      }
+    }
+
+    // XXX: log
+    bmp_tlv_list_destroy(tlvs);
+
+    (*len) -= orig_loc_len;
+    telen -= elen;
+  }
+}
+
+void bmp_rpat_common_hdr_get_v_flag(struct bmp_rpat_common_hdr *brch, u_int8_t *family)
+{
+  u_int8_t version;
+
+  if (brch && family) {
+    version = (brch->flags & BMP_PEER_FLAGS_ARI_V);
+    (*family) = FALSE;
+
+    if (version == 0) (*family) = AF_INET;
+    else (*family) = AF_INET6;
+  }
+}
+
+void bmp_rpat_common_hdr_get_bgp_id(struct bmp_rpat_common_hdr *brch, struct host_addr *a)
+{
+  if (brch && a) {
+    a->family = AF_INET;
+    a->address.ipv4.s_addr = brch->bgp_id;
+  }
+}
+
+void bmp_rpat_common_hdr_get_prefix(struct bmp_rpat_common_hdr *brch, struct host_addr *a, u_int8_t *family)
+{
+  if (brch && a) {
+    if ((*family) == AF_INET) a->address.ipv4.s_addr = brch->prefix[3];
+    else if ((*family) == AF_INET6) memcpy(&a->address.ipv6, &brch->prefix, 16);
+    else {
+      memset(a, 0, sizeof(struct host_addr));
+      if (!brch->prefix[0] && !brch->prefix[1] && !brch->prefix[2] && !brch->prefix[3]) {
+        (*family) = AF_INET; /* we just set this up to something non-zero */
+      }
+    }
+
+    a->family = (*family);
+  }
+}
+
+void bmp_rpat_common_hdr_get_prefix_len(struct bmp_rpat_common_hdr *brch, u_int8_t *plen)
+{
+  if (brch && plen) (*plen) = brch->prefix_len;
+}
+
+void bmp_rpat_event_hdr_get_index(struct bmp_rpat_event_hdr *breh, u_int8_t *idx)
+{
+  if (breh && idx) (*idx) = breh->idx;
+}
+
+void bmp_rpat_event_hdr_get_tstamp(struct bmp_rpat_event_hdr *breh, struct timeval *tv)
+{
+  u_int32_t sec, usec;
+
+  if (breh && tv) {
+    if (breh->tstamp_sec) {
+      sec = ntohl(breh->tstamp_sec);
+      usec = ntohl(breh->tstamp_usec);
+
+      tv->tv_sec = sec;
+      tv->tv_usec = usec;
+    }
+  }
+}
+
+void bmp_rpat_event_hdr_get_path_id(struct bmp_rpat_event_hdr *breh, u_int32_t *path_id)
+{
+  if (breh && path_id) (*path_id) = ntohl(breh->path_id);
+}
+
+void bmp_rpat_event_hdr_get_afi_safi(struct bmp_rpat_event_hdr *breh, afi_t *afi, safi_t *safi)
+{
+  if (breh) {
+    if (afi && safi) {
+      (*afi) = ntohs(breh->afi);
+      (*safi) = breh->safi;
+    }
+  }
+}

src/bmp/bmp_rpat.h
@@ -1,0 +1,68 @@
+/*
+    pmacct (Promiscuous mode IP Accounting package)
+    pmacct is Copyright (C) 2003-2020 by Paolo Lucente
+*/
+
+/*
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
+
+#ifndef BMP_RPAT_H
+#define BMP_RPAT_H
+
+/* defines */
+struct bmp_rpat_common_hdr {
+  u_char        flags;
+  u_char        rd[RD_LEN];
+  u_char	prefix_len;
+  u_int32_t     prefix[4];
+  u_int32_t     bgp_id;
+  u_char	events_count;
+  u_int16_t	events_length;
+} __attribute__ ((packed));
+
+struct bmp_rpat_event_hdr {
+  u_int16_t	len;
+  u_int8_t	idx;
+  u_int32_t     tstamp_sec;
+  u_int32_t     tstamp_usec;
+  u_int32_t	path_id;
+  u_int16_t	afi;
+  u_int8_t	safi;
+} __attribute__ ((packed));
+
+struct bmp_log_rpat {
+  struct host_addr prefix;
+  u_int8_t prefix_len;
+  u_int8_t event_idx;
+  struct timeval tstamp;
+  u_int32_t path_id;
+  afi_t afi;
+  safi_t safi;
+};
+
+/* prototypes */
+extern void bmp_process_msg_rpat(char **, u_int32_t *, struct bmp_peer *);
+
+extern void bmp_rpat_common_hdr_get_v_flag(struct bmp_rpat_common_hdr *, u_int8_t *);
+extern void bmp_rpat_common_hdr_get_bgp_id(struct bmp_rpat_common_hdr *, struct host_addr *);
+extern void bmp_rpat_common_hdr_get_prefix(struct bmp_rpat_common_hdr *, struct host_addr *, u_int8_t *);
+extern void bmp_rpat_common_hdr_get_prefix_len(struct bmp_rpat_common_hdr *, u_int8_t *);
+
+extern void bmp_rpat_event_hdr_get_index(struct bmp_rpat_event_hdr *, u_int8_t *);
+extern void bmp_rpat_event_hdr_get_tstamp(struct bmp_rpat_event_hdr *, struct timeval *tv);
+extern void bmp_rpat_event_hdr_get_path_id(struct bmp_rpat_event_hdr *, u_int32_t *);
+extern void bmp_rpat_event_hdr_get_afi_safi(struct bmp_rpat_event_hdr *, afi_t *, safi_t *);
+#endif //BMP_RPAT_H

commit a1f1353ac92a42117bdcf209c09b7e39ad8abfaa

* fix, BMP daemon: commit 4761d67 caused a failed assertion BMP_MSG_TYPE_MAX < MAX_AVRO_SCHEMA. To address Issue #380

src/pmacct-build.h
@@ -1,1 +1,1 @@
-#define PMACCT_BUILD	"20200405-00"
+#define PMACCT_BUILD	"20200406-00"

src/pmacct-defines.h
@@ -39,7 +39,7 @@
 #define FOLLOW_BGP_NH_ENTRIES 32 
 #define MAX_PROTOCOL_LEN 32 
 #define DEFAULT_AVRO_SCHEMA_REFRESH_TIME 60
-#define MAX_AVRO_SCHEMA 16
+#define MAX_AVRO_SCHEMA 128
 #define DEFAULT_IMT_PLUGIN_POLL_TIMEOUT 5
 #define DEFAULT_SLOTH_SLEEP_TIME 5
 #define UINT32T_THRESHOLD 4290000000UL