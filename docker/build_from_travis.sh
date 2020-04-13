49d81e177eaf6baae019ef6ff48f9762147a5146
Claudio Ortega <claudio.alberto.ortega@gmail.com>
2020-04-13 11:40:20 -0700

    adapting to travis jobs - adding all/single variant to speed up builds

--- docker/build_from_travis.sh
+++ docker/build_from_travis.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
 LINUX_IMAGE_DISTRO=${1}
+VARIANT_SPEC=${2}
 
 if [[ ( ${LINUX_IMAGE_DISTRO} == "centos" ) ]]; then
   bash -ex docker/build_outside.sh \
     docker/Dockerfile-centos-8.1-for-pmacct \
     centos8.1-for-pmacct \
-    all \
+    ${VARIANT_SPEC} \
     master
 elif [[ ( ${LINUX_IMAGE_DISTRO} == "ubuntu" ) ]]; then
   bash -ex docker/build_outside.sh \
     docker/Dockerfile-ubuntu-bionic-for-pmacct \
     ubuntu-bionic-for-pmacct \
-    all \
+    ${VARIANT_SPEC} \
     master
 else
   echo "unsupported linux distribution: ["${LINUX_IMAGE_DISTRO}"]"
