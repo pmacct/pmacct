d44721e700e462bb7e1d7434798d90fff48533ff
Marc Sune <marcdevel@gmail.com>
2020-06-01 22:27:34 +0200

    ci: use dedicated jobs per {docker, config_flags}
    Prior to this commit there were only 2 jobs being launched, in which
    all possible combinations of pmacct flags were compiled.
    
    This had two fundamental problems:
    
    * Jobs could not run in parallel so could not exploit parallel jobs
      of travis CI (even in free tier)
    * If a combination was failing, the entire job was failing
    
    In addition all the log was in the same _very long_ job, making it
    difficult to debug.
    
    Unfortunately, parallel jobs is not improving pipeline times, since
    docker generation - which takes > 70% of time - cannot be reused
    across jobs.
    
    This should be addressed in a later commit, in which two different stages
    are defined.
    
    This commit builds on top of:
    
    * Moves dependencies to Dockerfile
    * Moves all related files from docker/ to ci/. This is to avoid users
      thinking that docker file is a pmacct docker => btw this might be
      a good idea; someone thought of adding a pmacct public layer?
    * Refactors CI to use a job per tuple {docker, config_flags}. It has
      a single stage and it prefixes all jobs with name:
       - Ubuntu Bionic
       - Centos 8.1
    * Tries to simplify the CI code
    * Adds minor trick to pmacct ./configure to show config.log in case
      of failure

--- ci/deps.sh
+++ ci/deps.sh
@@ -1,8 +1,7 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 #    Copyright
-#    (c) 2019-2020 Claudio Ortega <claudio.alberto.ortega@gmail.com>
-#    (c) 2019-2020 Paolo Lucente <paolo@pmacct.net>
+#    (c) 2020 Marc Sune <marcdevel@gmail.com>
 #
 #    Permission to use, copy, modify, and distribute this software for any
 #    purpose with or without fee is hereby granted, provided that the above
@@ -16,59 +15,43 @@
 #    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 #    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-exit_on_error() {
-	exit_code=$1
-	if [ $exit_code -ne 0 ]; then
-		exit $exit_code
-	fi
-}
+#Do not delete
+set -e
 
-BASE_DIR=$(pwd)
-DEPS_DIR="dependencies"
+#Don't pollute /
+mkdir -p /tmp
+cd /tmp
 
-rm -rf ${DEPS_DIR} && mkdir ${DEPS_DIR} && cd ${DEPS_DIR}
+#Dependencies (not fulfilled by Dockerfile)
 
 git clone https://github.com/akheron/jansson
 cd jansson && rm -rf ./.git && autoreconf -i && ./configure && make && sudo make install && cd ..
-exit_on_error $?
 
 git clone https://github.com/edenhill/librdkafka
 cd librdkafka && rm -rf ./.git && ./configure && make && sudo make install && cd ..
-exit_on_error $?
 
 git clone https://github.com/alanxz/rabbitmq-c
 cd rabbitmq-c && rm -rf ./.git && mkdir build && cd build && cmake -DCMAKE_INSTALL_LIBDIR=lib .. && sudo cmake --build . --target install && cd .. && cd ..
-exit_on_error $?
 
 git clone --recursive https://github.com/maxmind/libmaxminddb 
 cd libmaxminddb && rm -rf ./.git && ./bootstrap && ./configure && make && sudo make install && cd ..
-exit_on_error $?
 
 git clone -b 3.2-stable https://github.com/ntop/nDPI
 cd nDPI && rm -rf ./.git && ./autogen.sh && ./configure && make && sudo make install && sudo ldconfig && cd ..
-exit_on_error $?
 
 wget --no-check-certificate https://github.com/zeromq/libzmq/releases/download/v4.3.2/zeromq-4.3.2.tar.gz
 tar xfz zeromq-4.3.2.tar.gz
 cd zeromq-4.3.2 && ./configure && make && sudo make install && cd ..
-exit_on_error $?
 
 wget --no-check-certificate https://archive.apache.org/dist/avro/avro-1.9.2/c/avro-c-1.9.2.tar.gz
 tar xfz avro-c-1.9.2.tar.gz
 cd avro-c-1.9.2 && mkdir build && cd build && cmake .. && make && sudo make install && cd .. && cd ..
-exit_on_error $?
 
 git clone https://github.com/confluentinc/libserdes
 cd libserdes && rm -rf ./.git && ./configure && make && sudo make install && cd ..
-exit_on_error $?
 
 git clone https://github.com/redis/hiredis
 cd hiredis && rm -rf ./.git && make && sudo make install && cd ..
-exit_on_error $?
 
-cd ..
-
-echo "pwd:"$(pwd)
-echo "BASE_DIR:"$BASE_DIR
-
-exit 0
+#Make sure dynamic linker is up-to-date
+ldconfig
