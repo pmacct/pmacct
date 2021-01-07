8583beaadc4b8d559b84edc0f7273e59aea842d8
Paolo Lucente <pl+github@pmacct.net>
2021-01-07 21:47:48 +0000

    * Updated docs.

--- src/cfg.c
+++ src/cfg.c
@@ -70,8 +70,6 @@ static const struct _dictionary_line dictionary[] = {
   {"plugin_pipe_zmq_profile", cfg_key_plugin_pipe_zmq_profile},
   {"plugin_pipe_zmq_hwm", cfg_key_plugin_pipe_zmq_hwm},
   {"plugin_exit_any", cfg_key_plugin_exit_any},
-  {"interface", cfg_key_pcap_interface}, 		/* Legacy key */
-  {"interface_wait", cfg_key_pcap_interface_wait},	/* Legacy key */
   {"files_umask", cfg_key_files_umask},
   {"files_uid", cfg_key_files_uid},
   {"files_gid", cfg_key_files_gid},
