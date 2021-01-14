pmacct/pmacct

commit 707442dbe05c8f05c50551f8da77d4866402e78a

* fix, pmacct-defines.h: long due distinguish among PM_FTYPE_TRAFFIC (generic) and PM_FTYPE_IPV4 (v4 only)

src/nfacctd.c
@@ -1,6 +1,6 @@
 /*  
     pmacct (Promiscuous mode IP Accounting package)
-    pmacct is Copyright (C) 2003-2020 by Paolo Lucente
+    pmacct is Copyright (C) 2003-2021 by Paolo Lucente
 */
 
 /*
@@ -2875,7 +2875,7 @@ void NF_compute_once()
 
 u_int8_t NF_evaluate_flow_type(struct template_cache_entry *tpl, struct packet_ptrs *pptrs)
 {
-  u_int8_t ret = PM_FTYPE_TRAFFIC;
+  u_int8_t ret = FALSE;
   u_int8_t have_ip_proto = FALSE;
 
   /* first round: event vs traffic */
@@ -2886,26 +2886,36 @@ u_int8_t NF_evaluate_flow_type(struct template_cache_entry *tpl, struct packet_p
   }
   else {
     if ((tpl->tpl[NF9_IN_VLAN].len && *(pptrs->f_data+tpl->tpl[NF9_IN_VLAN].off) > 0) ||
-        (tpl->tpl[NF9_OUT_VLAN].len && *(pptrs->f_data+tpl->tpl[NF9_OUT_VLAN].off) > 0)) ret += PM_FTYPE_VLAN;
-    if (tpl->tpl[NF9_MPLS_LABEL_1].len /* check: value > 0 ? */) ret += PM_FTYPE_MPLS;
+        (tpl->tpl[NF9_OUT_VLAN].len && *(pptrs->f_data+tpl->tpl[NF9_OUT_VLAN].off) > 0)) {
+      ret += PM_FTYPE_VLAN;
+    }
+
+    if (tpl->tpl[NF9_MPLS_LABEL_1].len /* check: value > 0 ? */) {
+      ret += PM_FTYPE_MPLS;
+    }
 
     /* Explicit IP protocol definition first; a bit of heuristics as fallback */
     if (tpl->tpl[NF9_IP_PROTOCOL_VERSION].len) {
       if (*(pptrs->f_data+tpl->tpl[NF9_IP_PROTOCOL_VERSION].off) == 4) {
+	ret += PM_FTYPE_IPV4;
 	have_ip_proto = TRUE;
       }
       else if (*(pptrs->f_data+tpl->tpl[NF9_IP_PROTOCOL_VERSION].off) == 6) {
-	ret += PM_FTYPE_TRAFFIC_IPV6;
+	ret += PM_FTYPE_IPV6;
 	have_ip_proto = TRUE;
       }
+    }
+    else {
+      ret += PM_FTYPE_IPV4;
     }
 
     if (!have_ip_proto) {
       if (tpl->tpl[NF9_IPV4_SRC_ADDR].len) {
+        ret += PM_FTYPE_IPV4;
 	have_ip_proto = TRUE;
       }
       else if (tpl->tpl[NF9_IPV6_SRC_ADDR].len) {
-	ret += PM_FTYPE_TRAFFIC_IPV6;
+	ret += PM_FTYPE_IPV6;
 	have_ip_proto = TRUE;
       }
     }

src/nl.c
@@ -1,6 +1,6 @@
 /*
     pmacct (Promiscuous mode IP Accounting package)
-    pmacct is Copyright (C) 2003-2020 by Paolo Lucente
+    pmacct is Copyright (C) 2003-2021 by Paolo Lucente
 */
 
 /*
@@ -354,6 +354,11 @@ int ip_handler(register struct packet_ptrs *pptrs)
   }
 
   quit:
+
+  if (ret) {
+    pptrs->flow_type = PM_FTYPE_IPV4;
+  }
+ 
   return ret;
 }
 
@@ -514,7 +519,12 @@ int ip6_handler(register struct packet_ptrs *pptrs)
   }
 
   quit:
-  return TRUE;
+
+  if (ret) {
+    pptrs->flow_type = PM_FTYPE_IPV6;
+  }
+
+  return ret;
 }
 
 int PM_find_id(struct id_table *t, struct packet_ptrs *pptrs, pm_id_t *tag, pm_id_t *tag2)

src/pmacct-defines.h
@@ -527,23 +527,22 @@ typedef u_int64_t pm_counter_t;
 #define NF_NET_FALLBACK	0x80000000 /* Fallback flag */
 
 /* flow type */
-#define PM_FTYPE_TRAFFIC		1  /* temporary: re-coding needed */
-#define PM_FTYPE_TRAFFIC_IPV6		1
-#define PM_FTYPE_IPV4			1
-#define PM_FTYPE_IPV6			2
+#define PM_FTYPE_TRAFFIC		1
+#define PM_FTYPE_IPV4			2
+#define PM_FTYPE_IPV6			3
 #define PM_FTYPE_VLAN			5
-#define PM_FTYPE_VLAN_IPV4		6  /* PM_FTYPE_VLAN + PM_FTYPE_IPV4 */
-#define PM_FTYPE_VLAN_IPV6		7  /* PM_FTYPE_VLAN + PM_FTYPE_IPV6 */
+#define PM_FTYPE_VLAN_IPV4		7  /* PM_FTYPE_VLAN + PM_FTYPE_IPV4 */
+#define PM_FTYPE_VLAN_IPV6		8  /* PM_FTYPE_VLAN + PM_FTYPE_IPV6 */
 #define PM_FTYPE_MPLS			10
-#define PM_FTYPE_MPLS_IPV4		11 /* PM_FTYPE_MPLS + PM_FTYPE_IPV4 */
-#define PM_FTYPE_MPLS_IPV6		12 /* PM_FTYPE_MPLS + PM_FTYPE_IPV6 */
+#define PM_FTYPE_MPLS_IPV4		12 /* PM_FTYPE_MPLS + PM_FTYPE_IPV4 */
+#define PM_FTYPE_MPLS_IPV6		13 /* PM_FTYPE_MPLS + PM_FTYPE_IPV6 */
 #define PM_FTYPE_VLAN_MPLS		15 /* PM_FTYPE_MPLS + PM_FTYPE_VLAN */
-#define PM_FTYPE_VLAN_MPLS_IPV4		16 /* PM_FTYPE_VLAN_MPLS + PM_FTYPE_IPV4 */
-#define PM_FTYPE_VLAN_MPLS_IPV6		17 /* PM_FTYPE_VLAN_MPLS + PM_FTYPE_IPV6 */
+#define PM_FTYPE_VLAN_MPLS_IPV4		17 /* PM_FTYPE_VLAN_MPLS + PM_FTYPE_IPV4 */
+#define PM_FTYPE_VLAN_MPLS_IPV6		18 /* PM_FTYPE_VLAN_MPLS + PM_FTYPE_IPV6 */
 #define PM_FTYPE_TRAFFIC_MAX		99  /* temporary: re-coding needed */
 
 /* flow type: NetFlow/IPFIX extended code-points */
-#define NF9_FTYPE_DLFS			3
+#define NF9_FTYPE_DLFS			4
 #define NF9_FTYPE_EVENT			100 /* temporary: re-coding needed */
 #define NF9_FTYPE_NAT_EVENT             100
 #define NF9_FTYPE_OPTION		200

src/sfacctd.c
@@ -1,6 +1,6 @@
 /*  
     pmacct (Promiscuous mode IP Accounting package)
-    pmacct is Copyright (C) 2003-2020 by Paolo Lucente
+    pmacct is Copyright (C) 2003-2021 by Paolo Lucente
 */
 
 /*
@@ -2141,12 +2141,13 @@ int SF_find_id(struct id_table *t, struct packet_ptrs *pptrs, pm_id_t *tag, pm_i
 u_int8_t SF_evaluate_flow_type(struct packet_ptrs *pptrs)
 {
   SFSample *sample = (SFSample *)pptrs->f_data;
-  u_int8_t ret = PM_FTYPE_TRAFFIC;
+  u_int8_t ret = FALSE;
 
   if (sample->in_vlan || sample->out_vlan) ret += PM_FTYPE_VLAN;
   if (sample->lstk.depth > 0) ret += PM_FTYPE_MPLS;
-  if (sample->gotIPV4); 
-  else if (sample->gotIPV6) ret += PM_FTYPE_TRAFFIC_IPV6;
+
+  if (sample->gotIPV4) ret += PM_FTYPE_IPV4;
+  else if (sample->gotIPV6) ret += PM_FTYPE_IPV6;
 
   return ret;
 }

commit 9ccbcdcd994c2ef2608acbc777350972625a1fd0

* fix, nfacctd: improved NetFlow/IPFIX heuristics ( bug introduced with 707442dbe05c8f05c50551f8da77d4866402e78a ). Thanks to @tbearma1 for reporting the issue.

src/nfacctd.c
@@ -2903,16 +2903,13 @@ u_int8_t NF_evaluate_flow_type(struct template_cache_entry *tpl, struct packet_p
 	have_ip_proto = TRUE;
       }
     }
-    else {
-      ret += PM_FTYPE_IPV4;
-    }
 
     if (!have_ip_proto) {
-      if (tpl->tpl[NF9_IPV4_SRC_ADDR].len) {
+      if (tpl->tpl[NF9_IPV4_SRC_ADDR].len || tpl->tpl[NF9_IPV4_DST_ADDR].len) {
         ret += PM_FTYPE_IPV4;
 	have_ip_proto = TRUE;
       }
-      else if (tpl->tpl[NF9_IPV6_SRC_ADDR].len) {
+      else if (tpl->tpl[NF9_IPV6_SRC_ADDR].len || tpl->tpl[NF9_IPV6_DST_ADDR].len) {
 	ret += PM_FTYPE_IPV6;
 	have_ip_proto = TRUE;
       }