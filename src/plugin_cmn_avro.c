c4da8169e7ea757335ab826b29b91cb072c0db5e
Salvatore Cuzzilla <salvatore@cuzzilla.org>
2021-11-18 16:49:04 +0100

    tcpflags encoding for avro second attempt ...

--- src/plugin_cmn_avro.c
+++ src/plugin_cmn_avro.c
@@ -815,7 +815,7 @@ avro_value_t compose_avro_acct_data(u_int64_t wtc, u_int64_t wtc_2, u_int8_t flo
     sprintf(misc_str, "%u", tcp_flags);
 
     if (1) {
-      compose_tcpflag_avro_data(misc_str, value, FALSE);
+      compose_tcpflags_avro_data(misc_str, value, FALSE);
     }
     else {
       pm_avro_check(avro_value_get_by_name(&value, "tcp_flags", &field, NULL));
