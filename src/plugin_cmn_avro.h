6727eae2453aa4de960c8c60df1ff89a52181077
Salvatore Cuzzilla <salvatore@cuzzilla.org>
2021-12-13 16:38:49 +0100

    new forwarding_status primitive (3) ...

--- src/plugin_cmn_avro.h
+++ src/plugin_cmn_avro.h
@@ -65,7 +65,7 @@ extern avro_value_t compose_avro_acct_data(u_int64_t wtc, u_int64_t wtc_2, u_int
   struct pkt_tunnel_primitives *ptun, u_char *pcust,
   struct pkt_vlen_hdr_primitives *pvlen, pm_counter_t bytes_counter,
   pm_counter_t packet_counter, pm_counter_t flow_counter, u_int32_t tcp_flags, 
-  u_int32_t forwarding_status, struct timeval *basetime, struct pkt_stitching *stitch,
+  struct timeval *basetime, struct pkt_stitching *stitch,
   avro_value_iface_t *iface);
 extern avro_value_t compose_avro_acct_init(char *, pid_t, avro_value_iface_t *);
 extern avro_value_t compose_avro_acct_close(char *, pid_t, int, int, int, avro_value_iface_t *);
