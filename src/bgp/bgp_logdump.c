5ab10b17da5f99c9dc8118ad1e34fa36c690cad6
Salvatore Cuzzilla <salvatore@cuzzilla.org>
2022-01-15 20:06:50 +0100

    expose avro value & iface to test the decref - bxp (9) ...

--- src/bgp/bgp_logdump.c
+++ src/bgp/bgp_logdump.c
@@ -658,8 +658,8 @@ int bgp_peer_log_msg(struct bgp_node *route, struct bgp_info *ri, afi_t afi, saf
     avro_value_iface_decref(p_avro_iface);
     avro_writer_reset(p_avro_writer);
     avro_writer_free(p_avro_writer);
-    avro_value_decref(&v_type_union);
-    avro_value_iface_decref(if_type_union);
+    //avro_value_decref(&v_type_union);
+    //avro_value_iface_decref(if_type_union);
     
     if (bms->dump_kafka_avro_schema_registry) {
       free(p_avro_local_buf);
@@ -2413,7 +2413,7 @@ void bgp_tag_print_avro_decref(avro_value_iface_t *if_type_union, avro_value_t v
 
   if (tag->have_label) {
     if (config.pretag_label_encode_as_map) {
-      compose_label_avro_data_bxp(tag->label.val, if_type_union, v_type_union, obj);
+      compose_label_avro_data_bxp(tag->label.val, if_type_union, v_type_union, obgp_tag_print_avro_decref(if_type_union, v_type_union, p_avro_obj, tag);bj);
     }
     else {
       pm_avro_check(avro_value_get_by_name(&obj, "label", &p_avro_field, NULL));
