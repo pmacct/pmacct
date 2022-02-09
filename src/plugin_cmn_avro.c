66cb10ff957eac79dd13f3801eb75e0aad465e7a
Salvatore Cuzzilla <salvatore@cuzzilla.org>
2022-02-09 23:36:17 +0100

    IPFIX mplsLabelStack data structure for both JSON & AVRO encodings (3) ...

--- src/plugin_cmn_avro.c
+++ src/plugin_cmn_avro.c
@@ -866,7 +866,7 @@ avro_value_t compose_avro_acct_data(u_int64_t wtc, u_int64_t wtc_2, u_int8_t flo
       int idx_0;
       for(idx_0 = 0; idx_0 < MAX_MPLS_LABELS; idx_0++) {
         memset(&label_buf, 0, sizeof(label_buf));
-        snprintf(label_buf, MAX_MPLS_LABEL_LEN, "%lu", pmpls->labels_cycle[idx_0]);
+        snprintf(label_buf, MAX_MPLS_LABEL_LEN, "%u", pmpls->labels_cycle[idx_0]);
         strncat(mpls_label_stack, label_buf, (MAX_MPLS_LABEL_LEN - strlen(label_buf) - 1));
         strncat(mpls_label_stack, ",", (MAX_MPLS_LABEL_LEN - strlen(label_buf) - 1));
       }
@@ -1640,11 +1640,11 @@ int compose_nfacctd_mpls_label_stack_data(u_int32_t *labels_cycle, avro_value_t 
   size_t idx_0;
   for (idx_0 = 0; idx_0 < MAX_MPLS_LABELS; idx_0++) {
     memset(&label_buf, 0, sizeof(label_buf));
-    snprintf(label_buf, MAX_MPLS_LABEL_LEN, "%lu", *(labels_cycle + idx_0));
+    snprintf(label_buf, MAX_MPLS_LABEL_LEN, "%u", *(labels_cycle + idx_0));
     if (avro_value_get_by_name(&v_type_record, "mpls_label_stack", &v_type_array, NULL) == 0) {
       if (strncmp("0", label_buf, 1)) {
         memset(&idx_buf, 0, sizeof(idx_buf));
-        snprintf(idx_buf, MAX_IDX_LEN, "%lu", idx_0);
+        snprintf(idx_buf, MAX_IDX_LEN, "%zu", idx_0);
         strncat(label_buf, "-", (MAX_MPLS_LABEL_LEN - strlen(label_buf) - 1));
         strncat(label_buf, idx_buf, (MAX_MPLS_LABEL_LEN - strlen(label_buf) - 1));
         if (avro_value_append(&v_type_array, &v_type_string, NULL) == 0) {
