dc9f1a92beffc0b273e91bf05be69430d4a58d79
Salvatore Cuzzilla <salvatore@cuzzilla.org>
2022-02-17 16:45:44 +0100

    reversing mpls_lbl_stack indexing ...

--- src/plugin_cmn_avro.c
+++ src/plugin_cmn_avro.c
@@ -1632,7 +1632,9 @@ int compose_fwd_status_avro_data(size_t fwdstatus_decimal, avro_value_t v_type_r
 int compose_mpls_label_stack_data(u_int32_t *labels_cycle, avro_value_t v_type_record)
 {
   const int MAX_IDX_LEN = 4;
+  const int MAX_MPLS_LABEL_IDX_LEN = (MAX_IDX_LEN + MAX_MPLS_LABEL_LEN); 
   char label_buf[MAX_MPLS_LABEL_LEN];
+  char label_idx_buf[MAX_MPLS_LABEL_IDX_LEN];
   char idx_buf[MAX_IDX_LEN];
 
   avro_value_t v_type_array, v_type_string;
@@ -1644,11 +1646,13 @@ int compose_mpls_label_stack_data(u_int32_t *labels_cycle, avro_value_t v_type_r
     if (avro_value_get_by_name(&v_type_record, "mpls_label_stack", &v_type_array, NULL) == 0) {
       if (strncmp("0", label_buf, 1)) {
         memset(&idx_buf, 0, sizeof(idx_buf));
+        memset(&label_idx_buf, 0, sizeof(label_idx_buf));
         snprintf(idx_buf, MAX_IDX_LEN, "%zu", idx_0);
-        strncat(label_buf, "-", (MAX_MPLS_LABEL_LEN - strlen(label_buf) - 1));
-        strncat(label_buf, idx_buf, (MAX_MPLS_LABEL_LEN - strlen(label_buf) - 1));
+        strncat(label_idx_buf, idx_buf, (MAX_MPLS_LABEL_IDX_LEN - strlen(label_idx_buf) - 1));
+        strncat(label_idx_buf, "-", (MAX_MPLS_LABEL_IDX_LEN - strlen(label_idx_buf) - 1));
+        strncat(label_idx_buf, label_buf, (MAX_MPLS_LABEL_IDX_LEN - strlen(label_idx_buf) - 1));
         if (avro_value_append(&v_type_array, &v_type_string, NULL) == 0) {
-          avro_value_set_string(&v_type_string, label_buf);
+          avro_value_set_string(&v_type_string, label_idx_buf);
         }
       }
     }
