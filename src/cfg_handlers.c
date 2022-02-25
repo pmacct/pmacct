cba084a1aed789f4781cf824c23d7dda75776386
Raphaël P. Barazzutti <raphael@barazzutti.net>
2022-02-25 14:21:48 +0100

    update in config parsing and doc

--- src/cfg_handlers.c
+++ src/cfg_handlers.c
@@ -4926,8 +4926,8 @@ int cfg_key_bmp_daemon_dump_time_slots(char *filename, char *name, char *value_p
 
   value = atoi(value_ptr);
 
-  if (value < 1 || value > 1200) {
-    Log(LOG_ERR, "WARN: [%s] 'bmp_dump_time_slots' value has to be >= 1 and <= 1200.\n", filename);
+  if (value < 1 || value > 86400) {
+    Log(LOG_ERR, "WARN: [%s] 'bmp_dump_time_slots' value has to be >= 1 and <= 86400.\n", filename);
     return ERR;
   }
 
@@ -6965,8 +6965,8 @@ int cfg_key_bgp_daemon_table_dump_time_slots(char *filename, char *name, char *v
   }
 
   value = atoi(value_ptr);
-  if (value < 1 || value > 1200) {
-    Log(LOG_ERR, "WARN: [%s] 'bgp_table_dump_time_slots' value has to be >= 1 and <= 1200.\n", filename);
+  if (value < 1 || value > 86400) {
+    Log(LOG_ERR, "WARN: [%s] 'bgp_table_dump_time_slots' value has to be >= 1 and <= 86400.\n", filename);
     return ERR;
   }
 
