c22d8dbc0052a8bfd161bb8d63b9e428a2e798ed
Paolo Lucente <pl+github@pmacct.net>
2022-11-04 15:31:16 +0000

    * fix, PR #630 review: ha.[ch]: reworked header inclusions and addes copyright

--- src/ha.c
+++ src/ha.c
@@ -1,11 +1,27 @@
-// #include "pmacct-data.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/time.h>
+/*
+    pmacct (Promiscuous mode IP Accounting package)
+    pmacct is Copyright (C) 2003-2022 by Paolo Lucente
+*/
+
+/*
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
+
 #include "pmacct.h"
 #include "pmacct-data.h"
 #include "thread_pool.h"
-#include "cdada/queue.h"
 
 thread_pool_t *dq_pool;
 // A utility function to create a new linked list node.
--- src/ha.h
+++ src/ha.h
@@ -1,4 +1,26 @@
-#include "cdada/queue.h"
+/*
+    pmacct (Promiscuous mode IP Accounting package)
+    pmacct is Copyright (C) 2003-2022 by Paolo Lucente
+*/
+
+/*
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
+
+#ifndef HA_H
+#define HA_H
 
 /*Global variables*/
 typedef void (*queue_thread_handler)();
@@ -16,3 +38,5 @@ extern void pm_ha_countdown_delete();
 extern void pm_ha_queue_thread_wrapper();
 extern int pm_ha_queue_produce_thread(void *);
 extern void enQueue(cdada_queue_t*, void *, size_t);
+
+#endif //HA_H
