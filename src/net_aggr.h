75e158312172c13956c9a489957919aca2c66c26
Olivier Cochard-Labbé <olivier@cochard.me>
2023-02-28 22:09:18 +0100

    Fix build error with clang 15
    nfprobe_plugin.c:1521:16: error: passing arguments to 'set_net_funcs' without a prototype is deprecated in all versions of C and is not supported in C2x [-Werror,-Wdeprecated-non-prototype]

--- src/net_aggr.h
+++ src/net_aggr.h
@@ -91,7 +91,7 @@ struct networks_file_data {
 typedef void (*net_func) (struct networks_table *, struct networks_cache *, struct pkt_primitives *, struct pkt_bgp_primitives *, struct networks_file_data *);
 
 /* prototypes */
-extern void set_net_funcs();
+extern void set_net_funcs(struct networks_table *);
 extern void init_net_funcs(struct networks_table *, struct networks_cache *, struct pkt_primitives *, struct pkt_bgp_primitives *, struct networks_file_data *); 
 extern void mask_src_ipaddr(struct networks_table *, struct networks_cache *, struct pkt_primitives *, struct pkt_bgp_primitives *, struct networks_file_data *); 
 extern void mask_dst_ipaddr(struct networks_table *, struct networks_cache *, struct pkt_primitives *, struct pkt_bgp_primitives *, struct networks_file_data *); 
