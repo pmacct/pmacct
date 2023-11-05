2860946f7ab4475cabdf26c69524fb8c485214a9
Maxence Younsi <mx.yns@outlook.fr>
2023-11-05 16:34:37 +0100

    rename bgp_attr->flag to rpki_maxlen

--- src/bgp/bgp.h
+++ src/bgp/bgp.h
@@ -344,7 +344,7 @@ struct bgp_attr {
   struct ecommunity *ecommunity;
   struct lcommunity *lcommunity;
   unsigned long refcnt;
-  u_int8_t flag;
+  u_int8_t rpki_maxlen;
   struct in_addr nexthop;
   struct host_addr mp_nexthop;
   u_int32_t med;
