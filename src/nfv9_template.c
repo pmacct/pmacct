1efc82c6b3a71b1255d553c5d574ef3a33064382
Paolo Lucente <pl+github@pmacct.net>
2023-12-20 21:52:08 +0000

    * Updated docs

--- src/nfv9_template.c
+++ src/nfv9_template.c
@@ -1181,16 +1181,6 @@ struct template_cache_entry *handle_template_v2(struct template_hdr_v9 *hdr, str
     tpl = compose_opt_template(hdr, agent, tpl_type, sid, pens, version, len, seq);
   }
 
-/*
-  XXX: to be used in future when it will return value of the old record
-
-  ret = cdada_map_insert_replace(tpl_data_map, hash_keyval, tpl);
-  if (ret != CDADA_SUCCESS) {
-    Log(LOG_WARNING, "WARN ( %s/core ): Unable to insert / refresh template in tpl_data_map\n",
-	config.name);
-  }
-*/
-
   {
     void *old_tpl_aux = NULL;
 
