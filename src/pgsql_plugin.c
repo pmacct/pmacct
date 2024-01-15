pmacct/pmacct

commit 1c841728bde629fe4ad896081b275e40acff7e5d

remove password from log output

src/pgsql_plugin.c
@@ -803,7 +803,7 @@ void PG_compose_conn_string(struct DBdesc *db, char *host, int port, char *ca_fi
     }
     string = db->conn_string;
 
-    snprintf(string, slen, "dbname=%s user=%s password=%s", config.sql_db, config.sql_user, config.sql_passwd);
+    snprintf(string, slen, "dbname=%s user=%s", config.sql_db, config.sql_user);
     slen -= strlen(string);
     string += strlen(string);
 

commit d9e1b96276d6696a74d3d5df171e0b2a969f1e19

* fix, PostgreSQL plugin: bug introduced by 1c841728bde629fe4ad896081b275e40acff7e5d . To address issue #747

src/pgsql_plugin.c
@@ -1,6 +1,6 @@
 /*
     pmacct (Promiscuous mode IP Accounting package)
-    pmacct is Copyright (C) 2003-2023 by Paolo Lucente
+    pmacct is Copyright (C) 2003-2024 by Paolo Lucente
 */
 
 /*
@@ -810,7 +810,7 @@ void PG_compose_conn_string(struct DBdesc *db, char *host, int port, char *ca_fi
     }
     string = db->conn_string;
 
-    snprintf(string, slen, "dbname=%s user=%s", config.sql_db, config.sql_user);
+    snprintf(string, slen, "dbname=%s user=%s password=%s", config.sql_db, config.sql_user, config.sql_passwd);
     slen -= strlen(string);
     string += strlen(string);
 
@@ -871,6 +871,7 @@ void PG_DB_Connect(struct DBdesc *db, char *host)
       sql_db_fail(db);
       strcpy(errmsg, "Failed connecting to ");
       strcat(errmsg, db->conn_string);
+      remove_password(errmsg, "password");
       db->errmsg = errmsg;
       sql_db_errmsg(db);
     }

src/util.c
@@ -1,6 +1,6 @@
 /*
     pmacct (Promiscuous mode IP Accounting package)
-    pmacct is Copyright (C) 2003-2023 by Paolo Lucente
+    pmacct is Copyright (C) 2003-2024 by Paolo Lucente
 */
 
 /*
@@ -3793,4 +3793,22 @@ u_int8_t sampling_direction_str2id(char *sd_str)
   else if (!strcmp(sd_str, "e")) return SAMPLING_DIRECTION_EGRESS;
 
   return SAMPLING_DIRECTION_UNKNOWN;
+}
+
+void remove_password(char *str, char *pwdstr)
+{
+  char *ptr = NULL;
+
+  ptr = strstr(str, pwdstr);
+  if (ptr) {
+    ptr += strlen(pwdstr);
+
+    if ((*ptr) == '=') {
+      ptr++;
+      while (!isblank((*ptr)) && !((*ptr) == '\0')) {
+        (*ptr) = 'X';
+	ptr++;
+      }
+    }
+  }
 }

src/util.h
@@ -1,6 +1,6 @@
 /*
     pmacct (Promiscuous mode IP Accounting package)
-    pmacct is Copyright (C) 2003-2023 by Paolo Lucente
+    pmacct is Copyright (C) 2003-2024 by Paolo Lucente
 */
 
 /*
@@ -140,6 +140,7 @@ extern void reload_logs(char *);
 extern int is_empty_256b(void *, int);
 extern ssize_t pm_recv(int, void *, size_t, int, unsigned int);
 extern int ft2af(u_int8_t);
+extern void remove_password(char *, char *);
 
 extern char *compose_json_str(void *);
 extern void write_and_free_json(FILE *, void *);