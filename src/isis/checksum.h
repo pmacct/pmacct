f5ecb6c702769ba4adfd6f6806c19d24ed942a6e
Paolo Lucente <pl+github@pmacct.net>
2025-10-07 13:52:31 +0000

    * Removing ISIS daemon code (2)

--- src/isis/checksum.h
+++
@@ -1,7 +0,0 @@
-#ifndef _CHECKSUM_H_
-#define _CHECKSUM_H_
-
-extern int in_cksum(void *, int);
-extern u_int16_t fletcher_checksum(u_char *, const size_t, const uint16_t);
-
-#endif /* _CHECKSUM_H_ */
