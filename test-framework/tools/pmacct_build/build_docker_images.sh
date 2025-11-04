45543c171c2dab8891b59750e4a7cfea48d8c8da
Pedro-W21 <89784517+Pedro-W21@users.noreply.github.com>
2025-11-04 17:48:53 +0100

    proper script options and failing only if the lib isn't found when asking for one

--- test-framework/tools/pmacct_build/build_docker_images.sh
+++ test-framework/tools/pmacct_build/build_docker_images.sh
@@ -6,9 +6,28 @@ PMACCT_ROOT_LOCATION="$SCRIPT_DIR/../../../"
 
 TAG='_build'
 
-CUSTOM_LIB_PATH="libnothing.so"
+CUSTOM_LIB_PATH="ci/libnothing.so*"
 
 # Using this as a guide for options https://www.redhat.com/en/blog/arguments-options-bash-scripts
+
+# Get the options
+while getopts ":hp:" option; do
+   case $option in
+      h) # help
+         echo "This script builds all images required to run pmacct tests"
+         echo "options:"
+         echo "-h                        Print this help."
+         echo "-p  {path to library}     Build the images with the custom parsing library found locally at the specified path (the path must be relative to the root of the pmacct directory)."
+         exit;;
+      p) # custom Parsing lib
+         echo "Custom parsing lib detected : $OPTARG"
+         CUSTOM_LIB_PATH=$OPTARG;;
+     \?) # invalid option
+         echo "Error: Invalid option"
+         exit;;
+   esac
+done
+
 
 echo "Building pmacct docker images"
 docker build --build-arg NUM_WORKERS=$(nproc) --build-arg CUSTOM_LIB_PATH=$CUSTOM_LIB_PATH -t base:$TAG -f $PMACCT_ROOT_LOCATION/docker/base/Dockerfile $PMACCT_ROOT_LOCATION || exit $?
