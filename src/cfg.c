e132c1aeb08fe2f4e93a1038a1798eaf8ad11a32
Paolo Lucente <pl+github@pmacct.net>
2025-11-05 16:24:54 +0000

    * Updated docs

--- src/cfg.c
+++ src/cfg.c
@@ -88,6 +88,7 @@ static const struct _dictionary_line dictionary[] = {
   {"ports_file", cfg_key_ports_file},
   {"protos_file", cfg_key_protos_file},
   {"tos_file", cfg_key_tos_file},
+  {"timestamps_rfc3339", cfg_key_timestamps_rfc9554},
   {"timestamps_rfc9554", cfg_key_timestamps_rfc9554},
   {"timestamps_utc", cfg_key_timestamps_utc},
   {"timestamps_secs", cfg_key_timestamps_secs},
