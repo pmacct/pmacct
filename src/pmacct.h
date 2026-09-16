793f1fa6c47daf6e645ec4d983c36d284f459d55
Piotr Kubaj <pkubaj@FreeBSD.org>
2026-09-16 09:13:44 +0200

    pmacct.h: #undef ntoh*/hton* before redefining them on big-endian
    On big-endian platforms pmacct.h redefines ntohs/ntohl/htons/htonl as
    identity macros, but <netinet/in.h> (included earlier in the same header)
    already defines them.  Under -Werror=macro-redefined this fails to build,
    e.g. on FreeBSD/powerpc64.  #undef each macro before redefining it.

--- src/pmacct.h
+++ src/pmacct.h
@@ -106,9 +106,13 @@
 #endif
 
 #if defined IM_BIG_ENDIAN
+#undef ntohs
 #define ntohs(x) (x)
+#undef ntohl
 #define ntohl(x) (x)
+#undef htons
 #define htons(x) (x)
+#undef htonl
 #define htonl(x) (x)
 #endif
 
